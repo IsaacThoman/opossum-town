68b7eae92ab9613d1731af158d2b4ad67fa57ea7
IsaacThoman <isaacthoman@outlook.com>
2022-09-12 18:09:56 -0400

    placed roads

--- game.js
+++ game.js
@@ -3,10 +3,14 @@ const ctx = canvas.getContext("2d");
 let assets = {};
 let keys = {};
 let player = {x:0, y:0, baseSpeed:2 ,speed:0, speedDampened:0};
-let worldObjects =
+
 assets["opossum"] = new Asset('assets/opossum.webp', 80,44);
 assets["building"] = new Asset('assets/Untitled-1.png', 160,160);
 assets["bg"] = new Asset('assets/roadtest2.png', 1024,1024);
+
+for(let i = 1; i<=6; i++)
+    assets["road"+i] = new Asset('assets/roads/road'+i+'.png',42,42);
+
 function Asset(texture, w, h){
     this.texture = new Image(); this.texture.src = texture;
     this.w = w;
@@ -27,26 +31,46 @@ function WorldObject(asset, x, y, s){
 
 let frameOn=0;
 let xVals = [];
-let bg = new WorldObject(assets["bg"],0,0,1)
-function doFrame(){
+let bg = new WorldObject(assets["bg"],0,0,1);
+let worldObjects = [];
+for(let i = -3; i<3; i++) {
+    worldObjects.push(new WorldObject(assets["road2"],-42*3,i*42,1))
+    worldObjects.push(new WorldObject(assets["road2"],-42*10,i*42,1))
+    worldObjects.push(new WorldObject(assets["road1"],42*i-42*6,42*3,1))
+    worldObjects.push(new WorldObject(assets["road1"],42*i-42*6,-42*4,1))
 
+}
+worldObjects.push(new WorldObject(assets['road6'],-42*3,42*3,1))
+worldObjects.push(new WorldObject(assets['road3'],-42*3,-42*4,1))
+worldObjects.push(new WorldObject(assets['road4'],-42*10,42*3,1))
+worldObjects.push(new WorldObject(assets['road5'],-42*10,-42*4,1))
+
+function doFrame(){
+    clearDebug();
     updateRes();
     screen.x = player.x; screen.y = player.y;
     clearScreen();
     bg.draw(ctx);
 
+    for(let i = 0; i<worldObjects.length; i++)
+        worldObjects[i].draw(ctx);
+
     assets["opossum"].draw(ctx, screen.w/2, screen.h/2+(player.speedDampened*3/player.baseSpeed*Math.sin(frameOn/3)),1);
     player.speedDampened = player.speedDampened + (player.speed-player.speedDampened)*0.2;
 
-
+    addDebug('x: '+Math.floor(player.x) +' y: '+Math.floor(player.y));
 
 
 //xVals[frameOn] = player.x;
 //console.log(xVals[frameOn]-xVals[frameOn-15])
 
     processInputs();
+    showDebugText();
 
     frameOn++;
+    if(player.x>512)player.x=512; if(player.x<-512) player.x=-512;
+    if(player.y>512)player.y=512; if(player.y<-512)player.y=-512;
+
     requestAnimationFrame(doFrame);
 }
 requestAnimationFrame(doFrame);
@@ -67,17 +91,10 @@ function updateRes(){
 }
 
 function clearScreen(){
-    ctx.fillStyle = "#6c6c6c";
+    ctx.fillStyle = "#211303";
     ctx.beginPath();
     ctx.fillRect(0,0,10000,10000);
     ctx.closePath();
-
-
-    ctx.fillStyle = "#a12929";
-    ctx.beginPath();
-    ctx.fillRect(0,0,42,42);
-    ctx.closePath();
-
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
@@ -114,5 +131,14 @@ function processInputs(){
         player.x += player.speedDampened*Math.cos(lastAngle);
         player.y += player.speedDampened*Math.sin(lastAngle);
 
-
+}
+var debugText = '';
+function clearDebug(){debugText = '';}
+function addDebug(text){debugText+=text+'\n'}
+function showDebugText(){
+    ctx.fillStyle = "#ff0000";
+    ctx.font = '14px';
+    let splitText = debugText.split('\n');
+    for(let i = 0; i<splitText.length; i++)
+        ctx.fillText(splitText[i], 2, 10*(1+i));
 }
